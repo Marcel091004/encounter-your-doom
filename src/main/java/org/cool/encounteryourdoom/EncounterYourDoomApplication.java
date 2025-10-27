7f8c6ad0ba8ea703a256bd70c68029b6f43a5ced
Marcel Link <marcel.link@datev.de>
2025-10-27 16:29:42 +0100

    initial commit

---
+++ src/main/java/org/cool/encounteryourdoom/EncounterYourDoomApplication.java
@@ -0,0 +1,13 @@
+package org.cool.encounteryourdoom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncounterYourDoomApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EncounterYourDoomApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/cool/encounteryourdoom/EncounterYourDoomApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cool.encounteryourdoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncounterYourDoomApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
