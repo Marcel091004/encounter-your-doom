279c6e1baf43f8b8d6aaaed056c5de3cd6b59344
Marcel Link <marcel.link@datev.de>
2025-10-30 13:56:59 +0100

    Add methods to retrieve all creatures with optional filters for region and rarity

--- src/main/java/org/cool/encounteryourdoom/Service/CreatureService.java
+++ src/main/java/org/cool/encounteryourdoom/Service/CreatureService.java
@@ -1,7 +1,14 @@
 package org.cool.encounteryourdoom.Service;
 
 import org.cool.encounteryourdoom.Repository.CreatureRepository;
+import org.cool.encounteryourdoom.model.CreatureEntity;
+import org.openapitools.model.Creature;
+import org.openapitools.model.Rarity;
+import org.openapitools.model.Region;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CreatureService {
@@ -13,4 +20,28 @@ public class CreatureService {
     CreatureService(CreatureRepository creatureRepository) {
         this.creatureRepository = creatureRepository;
     }
+
+    public List<CreatureEntity> getAllCreatures() {
+
+            return this.creatureRepository.findAll();
+    }
+
+    public List<CreatureEntity> getAllCreatures(Region region) {
+
+        if ()
+            return this.creatureRepository.findAll();
+    }
+
+    public List<CreatureEntity> getAllCreatures(Region region, Rarity rarity, String CR) {
+
+        if ()
+            return this.creatureRepository.findAll();
+    }
+
+    public List<CreatureEntity> getAllCreatures(Region region, Rarity rarity, String CR) {
+
+        if ()
+        return this.creatureRepository.findAll();
+    }
+
 }
