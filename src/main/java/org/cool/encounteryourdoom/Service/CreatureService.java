8035ccfc091d2e640cb401c618ab3c7d7a05430f
Heumann, Christian O31111 <Heumann.Christian+datev@datev.de>
2025-10-30 15:05:57 +0100

    added get all in CreatureService

--- src/main/java/org/cool/encounteryourdoom/Service/CreatureService.java
+++ src/main/java/org/cool/encounteryourdoom/Service/CreatureService.java
@@ -2,46 +2,55 @@ package org.cool.encounteryourdoom.Service;
 
 import org.cool.encounteryourdoom.Repository.CreatureRepository;
 import org.cool.encounteryourdoom.model.CreatureEntity;
-import org.openapitools.model.Creature;
 import org.openapitools.model.Rarity;
 import org.openapitools.model.Region;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class CreatureService {
 
-    //TODO : Implement CreatureService
+	//TODO : Implement CreatureService
 
-    private final CreatureRepository creatureRepository;
+	private final CreatureRepository creatureRepository;
 
-    CreatureService(CreatureRepository creatureRepository) {
-        this.creatureRepository = creatureRepository;
-    }
+	CreatureService(CreatureRepository creatureRepository) {
+		this.creatureRepository = creatureRepository;
+	}
 
-    public List<CreatureEntity> getAllCreatures() {
+	//TODO: Refactoring für weniger Methoden und einfache erweiterbarkeit (eigenes Interface?)
+	public List<CreatureEntity> getAllCreatures() {
+		return this.creatureRepository.findAll();
+	}
 
-            return this.creatureRepository.findAll();
-    }
+	public List<CreatureEntity> getAllCreatures(Region region) {
+		return this.creatureRepository.findByRegion(region);
+	}
 
-    public List<CreatureEntity> getAllCreatures(Region region) {
+	public List<CreatureEntity> getAllCreatures(Region region, Rarity rarity) {
+		return creatureRepository.findByRegionAndRarity(region, rarity);
+	}
 
-        if ()
-            return this.creatureRepository.findAll();
-    }
+	public List<CreatureEntity> getAllCreatures(Region region, String cr) {
+		return creatureRepository.findByRegionAndCr(region, cr);
+	}
 
-    public List<CreatureEntity> getAllCreatures(Region region, Rarity rarity, String CR) {
+	public List<CreatureEntity> getAllCreatures(Region region, Rarity rarity, String cr) {
+		return creatureRepository.findByRegionAndRarityAndCr(region, rarity, cr);
+	}
 
-        if ()
-            return this.creatureRepository.findAll();
-    }
+	public List<CreatureEntity> getAllCreatures(Rarity rarity) {
+		return creatureRepository.findByRarity(rarity);
+	}
 
-    public List<CreatureEntity> getAllCreatures(Region region, Rarity rarity, String CR) {
+	public List<CreatureEntity> getAllCreatures(Rarity rarity, String cr) {
+		return creatureRepository.findByRarityAndCr(rarity, cr);
+	}
 
-        if ()
-        return this.creatureRepository.findAll();
-    }
+	public List<CreatureEntity> getAllCreatures(String cr) {
+		return creatureRepository.findByCr(cr);
+	}
+
 
 }
