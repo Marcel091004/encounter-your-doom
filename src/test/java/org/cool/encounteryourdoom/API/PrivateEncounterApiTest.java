ada8c163941ab188d67bb47ff07e29f0b2e723cc
Heumann, Christian O31111 <Heumann.Christian+datev@datev.de>
2025-11-17 11:27:29 +0100

    Refactor PrivateEncounterApiTest to use MockMvc and improve test structure

--- src/test/java/org/cool/encounteryourdoom/API/PrivateEncounterApiTest.java
+++ src/test/java/org/cool/encounteryourdoom/API/PrivateEncounterApiTest.java
@@ -1,42 +1,53 @@
 package org.cool.encounteryourdoom.API;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.cool.encounteryourdoom.controller.PrivateEncounterController;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.openapitools.api.PrivateEncounterApi;
+import org.openapitools.model.Encounter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.request.NativeWebRequest;
 
-import java.util.Optional;
-
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PrivateEncounterApiTest {
 
+	@Autowired
 	private MockMvc mockMvc;
 
-	@BeforeEach
-	void setup() {
-	    mockMvc = MockMvcBuilders.standaloneSetup(new PrivateEncounterApi() {
-		}).build();
-	}
+	@MockitoBean
+	private PrivateEncounterController privateEncounterController;
 
 	@Nested
 	class GetPrivateEncounters {
 		@Test
 		void getsPrivateEncountersWith200Ok() throws Exception {
+
+			Encounter privateEncounter = new Encounter();
+			when(privateEncounterController.getEncounterForUser(any(), any())).thenReturn(ResponseEntity.ok(privateEncounter));
+
 			String userid = "123e4567-e89b-12d3-a456-426614174000";
 
 			mockMvc.perform(get("/datev/v1/privateEncounter/" + userid))
 					.andExpect(status().isOk());
 		}
+
 		@Test
 		void returns400BadRequestWhenGivenInvalidUUID() throws Exception {
+
+			when(privateEncounterController.getEncounterForUser(any(), any())).thenReturn(ResponseEntity.badRequest().build());
+
 			String userid = "invalid-uuid";
 
 			mockMvc.perform(get("/datev/v1/privateEncounter/" + userid))
