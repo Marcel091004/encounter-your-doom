8beee702b52fe7fb3cf558b5d9e14de05e0905a3
Heumann, Christian O31111 <Heumann.Christian+datev@datev.de>
2025-11-19 15:03:20 +0100

    Enhance PrivateEncounter API with new endpoints for creating, updating, and starting encounters, and improve related tests

--- src/main/java/org/cool/encounteryourdoom/service/PrivateEncounterService.java
+++ src/main/java/org/cool/encounteryourdoom/service/PrivateEncounterService.java
@@ -18,9 +18,10 @@ public class PrivateEncounterService {
 	private final PrivateEncounterMapper privateEncounterMapper;
 	private ActiveEncounterService activeEncounterService;
 
-	public PrivateEncounterService(PrivateEncounterRepository privateEncounterRepository, PrivateEncounterMapper privateEncounterMapper) {
+	public PrivateEncounterService(PrivateEncounterRepository privateEncounterRepository, PrivateEncounterMapper privateEncounterMapper, ActiveEncounterService activeEncounterService) {
 		this.privateEncounterRepository = privateEncounterRepository;
 		this.privateEncounterMapper = privateEncounterMapper;
+		this.activeEncounterService = activeEncounterService;
 	}
 
 	public List<EncounterEntity> getEncounterList(PrivateEncounterParameterFilter filter) {
@@ -47,5 +48,12 @@ public class PrivateEncounterService {
 		activeEncounterService.createActiveEncounterForUser(privateEncounterEntity.getUserId(), encounterId);
 	}
 
+	public void createPrivateEncounter(UUID userId, Encounter encounter) {
+		PrivateEncounterEntity privateEncounterEntity = privateEncounterMapper.toPrivatEncounterEntity(encounter);
 
+		privateEncounterEntity.setId(UUID.randomUUID());
+		privateEncounterEntity.setUserId(userId);
+
+		privateEncounterRepository.save(privateEncounterEntity);
+	}
 }
--- src/test/java/org/cool/encounteryourdoom/API/PrivateEncounterApiTest.java
+++ src/test/java/org/cool/encounteryourdoom/API/PrivateEncounterApiTest.java
@@ -1,5 +1,6 @@
 package org.cool.encounteryourdoom.API;
 
+import org.cool.encounteryourdoom.TestDataHelper;
 import org.cool.encounteryourdoom.controller.PrivateEncounterController;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -13,9 +14,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.net.URI;
+import java.util.UUID;
+
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
@@ -51,6 +55,129 @@ public class PrivateEncounterApiTest {
 			String userid = "invalid-uuid";
 
 			mockMvc.perform(get("/datev/v1/privateEncounter/" + userid))
+					.andExpect(status().isBadRequest());
+		}
+	}
+
+	@Nested
+	class GetPrivateEncounterById {
+		@Test
+		void getsPrivateEncounterByIdWith200Ok() throws Exception {
+
+			Encounter privateEncounter = new Encounter();
+			when(privateEncounterController.getEncounterForUser(any(), any())).thenReturn(ResponseEntity.ok(privateEncounter));
+
+			UUID userid = UUID.randomUUID();
+			UUID encounterid = UUID.randomUUID();
+
+			mockMvc.perform(get("/datev/v1/privateEncounter/" + encounterid + "/user/" + userid))
+					.andExpect(status().isOk());
+		}
+
+		@Test
+		void returns400BadRequestWhenGivenInvalidUUID() throws Exception {
+
+			when(privateEncounterController.getEncounterForUser(any(), any())).thenReturn(ResponseEntity.badRequest().build());
+
+			String userid = "apfel";
+			String encounterid = "apfelbaum";
+
+			mockMvc.perform(get("/datev/v1/privateEncounter/" + encounterid + "/user/" + userid))
+					.andExpect(status().isBadRequest());
+		}
+	}
+
+	@Nested
+	class UpdatePrivateEncounter {
+		@Test
+		void updatesPrivateEncounterWith204NoContent() throws Exception {
+
+			when(privateEncounterController.updateEncounterForUser(any(), any(), any())).thenReturn(ResponseEntity.noContent().build());
+
+			UUID userid = UUID.randomUUID();
+			UUID encounterid = UUID.randomUUID();
+			String content = TestDataHelper.getEncounterJson();
+
+			mockMvc.perform(put("/datev/v1/privateEncounter/" + encounterid + "/user/" + userid)
+							.contentType("application/json")
+							.content(content)
+					)
+					.andExpect(status().isNoContent());
+		}
+
+		@Test
+		void returns400BadRequestWhenGivenInvalidUUID() throws Exception {
+
+			when(privateEncounterController.updateEncounterForUser(any(), any(), any())).thenReturn(ResponseEntity.badRequest().build());
+
+			String userid = "invalid-uuid";
+			String encounterid = "invalid-uuid";
+			String content = TestDataHelper.getEncounterJson();
+
+			mockMvc.perform(put("/datev/v1/privateEncounter/" + encounterid + "/user/" + userid)
+							.contentType("application/json")
+							.content(content)
+					)
+					.andExpect(status().isBadRequest());
+		}
+	}
+
+	@Nested
+	class StartPrivateEncounter {
+		@Test
+		void startsPrivateEncounterWith204NoContent() throws Exception {
+
+			when(privateEncounterController.startEncounterForUser(any(), any())).thenReturn(ResponseEntity.noContent().build());
+
+			UUID userid = UUID.randomUUID();
+			UUID encounterid = UUID.randomUUID();
+
+			mockMvc.perform(post("/datev/v1/privateEncounter/" + encounterid + "/user/" + userid))
+					.andExpect(status().isNoContent());
+		}
+
+		@Test
+		void returns400BadRequestWhenGivenInvalidUUID() throws Exception {
+
+			when(privateEncounterController.startEncounterForUser(any(), any())).thenReturn(ResponseEntity.badRequest().build());
+
+			String userid = "invalid-uuid";
+			String encounterid = "invalid-uuid";
+
+			mockMvc.perform(post("/datev/v1/privateEncounter/" + encounterid + "/user/" + userid))
+					.andExpect(status().isBadRequest());
+		}
+	}
+
+	@Nested
+	class CreatePrivateEncounter {
+		@Test
+		void createsPrivateEncounterWith201Created() throws Exception {
+
+			when(privateEncounterController.createEncounterForUser(any(), any())).thenReturn(ResponseEntity.created(new URI("Test")).build());
+
+			UUID userid = UUID.randomUUID();
+			String content = TestDataHelper.getEncounterJson();
+
+			mockMvc.perform(post("/datev/v1/privateEncounter/" + userid)
+							.contentType("application/json")
+							.content(content)
+					)
+					.andExpect(status().isCreated());
+		}
+
+		@Test
+		void returns400BadRequestWhenGivenInvalidUUID() throws Exception {
+
+			when(privateEncounterController.createEncounterForUser(any(), any())).thenReturn(ResponseEntity.badRequest().build());
+
+			String userid = "invalid-uuid";
+			String content = TestDataHelper.getEncounterJson();
+
+			mockMvc.perform(post("/datev/v1/privateEncounter/" + userid)
+							.contentType("application/json")
+							.content(content)
+					)
 					.andExpect(status().isBadRequest());
 		}
 	}
